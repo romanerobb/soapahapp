128207bf8e38b295bae67a8c5de1057197c8b7dd
Romane Orlando Robb <mail@romanerobb.com>
2025-01-29 16:47:54 -0500

    Updated

--- php/newsletter.php
+++ php/newsletter.php
@@ -1,7 +1,7 @@
 <?php
 $field_email = $_POST['email'];
 
-$mail_to = 'to@email.com';
+$mail_to = 'biz@soapahapp.com';
 $subject = 'Message from a site visitor '.$field_email;
 
 $body_message .= 'E-mail: '.$field_email."\n";
